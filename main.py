d1a5466a7517fcb4d314f7299d044f03aaa1894e
Michał Hewelt <mhewelt9@gmail.com>
2024-01-02 17:59:58 +0100

    create password

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = []
+
+for n in range(nr_letters):
+    password += random.choice(letters)
+for n in range(nr_symbols):
+    password += random.choice(symbols)
+for n in range(nr_numbers):
+    password += random.choice(numbers)
+
+
